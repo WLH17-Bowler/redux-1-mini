aa7ab5d11e6e333eddf28cfb99ed2864aab3959a
Josh Bowler <joshbowler46@gmail.com>
2020-04-01 18:23:28 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
 import Counter from './Counter';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   render() {
-    return <Counter />;
+    return (
+      <div>
+        <Counter />
+      </div>
+    )      
   }
 }
-
-export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,63 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import {RESET, INCREMENT, DECREMENT, UNDO, REDO} from './store'
+import store from './store'
 
-class Counter extends Component {
+export default class Counter extends Component {
   constructor(props) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {
+      store: store.getState()
+    }
+  }
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState({store: store.getState()})
+    })
+  }
+  reset = () => {
+    store.dispatch({type: RESET})
+  }
+  increment = (amount) => {
+    store.dispatch({amount, type: INCREMENT})
+  }
+  decrement = (amount) => {
+    store.dispatch({amount, type: DECREMENT})
+  }
+  undo = () => {
+    store.dispatch({type: UNDO})
+  }
+  redo = () => {
+    store.dispatch({type: REDO})
   }
   render() {
+    const {currentValue, futureValues, previousValues} = this.state.store
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{0}</h1>
+          <h1 className="counter__current-value">{currentValue}</h1>
           <div className="counter__button-wrapper">
-            <button
-              className="counter__button increment-one"
-              onClick={() => null}
-            >
-              +1
-            </button>
-            <button
-              className="counter__button increment-five"
-              onClick={() => null}
-            >
-              +5
-            </button>
-            <button
-              className="counter__button decrement-one"
-              onClick={() => null}
-            >
-              -1
-            </button>
-            <button
-              className="counter__button decrement-five"
-              onClick={() => null}
-            >
-              -5
-            </button>
+            <button className="counter__button increment-one" onClick={() => this.increment(1)} > +1 </button>
+            <button className="counter__button increment-five" onClick={() => this.increment(5)} > +5 </button>
+            <button className="counter__button decrement-one" onClick={() => this.decrement(1)} > -1 </button>
+            <button className="counter__button decrement-five" onClick={() => this.decrement(5)} > -5 </button>
             <br />
-            <button
-              className="counter__button undo"
-              disabled={true}
-              onClick={() => null}
-            >
-              Undo
-            </button>
-            <button
-              className="counter__button redo"
-              disabled={true}
-              onClick={() => null}
-            >
-              Redo
-            </button>
+            <button className="counter__button undo" disabled={previousValues.length === 0} onClick={this.undo} > Undo </button>
+            <button className="counter__button redo" disabled={futureValues.length === 0} onClick={this.redo} > Redo </button>
+            <div>
+              <button className='counter_button reset' onClick={this.reset} > Reset </button>
+            </div>
           </div>
         </section>
         <section className="state">
           <pre>{JSON.stringify(this.state, null, 2)}</pre>
         </section>
       </div>
-    );
+    )
   }
 }
-
-export default Counter;
